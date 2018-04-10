cd16b62db04b6faf0e998b0b32dc73856a3fe980
Charles Thompson <thompsoncharles@live.com>
2018-04-10 11:40:56 +0000

    Finding numbers

---
+++ regex.py
@@ -0,0 +1,10 @@
+# Matching numbers only: ^[0-9]+$
+# ^[\d]+$
+
+import re
+
+with open('test.txt', 'r') as f:
+    test_string = f.read()
+    regex = re.compile(r'^([0-9]+)$', re.MULTILINE)
+    result = regex.findall(test_string)
+    print(result)
