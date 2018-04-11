c1284fbab80fa61d82e6f9b2583385d2a01046d9
Charles Thompson <thompsoncharles@live.com>
2018-04-11 01:15:59 +0000

    finding urls

--- regex.py
+++ regex.py
@@ -63,3 +63,13 @@ r'^([^@\s])+@[^@\s]+\.\w{2,6}$'
 # \*+ - Match one or more * characters
 # \/ - Match closing / symbol.
 r'(\/\*+)(.*)(\*+\/)'
+
+import re
+import urllib.request
+
+html = str(urllib.request.urlopen("https://moz.com/top500").read())
+regex = r"(https?:\/\/)(www\.)?(?P<domain>[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6})(?P<path>\/[-a-zA-Z0-9@:%_\/+.~#?&=]*)?"
+matches = re.finditer(regex, html)
+
+for match in matches:
+  print(match.group('domain'))
